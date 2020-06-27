293fe7e76147b4a51674a67c61d803f689e7f9fc
Lucas Viana <lucasviana112@gmail.com>
2020-06-27 16:11:27 -0300

    metodo que retorna todos agendamentos

--- src/repositories/AppointmentRepository.ts
+++ src/repositories/AppointmentRepository.ts
@@ -1,11 +1,20 @@
 import { isEqual } from 'date-fns';
 import Appointment from '../model/Appointment';
 
+interface CreateAppointmentDTO {
+  provider: string;
+  date: Date;
+}
+
 class AppointmentRepository {
   private appointments: Appointment[];
 
   constructor() {
     this.appointments = [];
+  }
+
+  public all(): Appointment[] {
+    return this.appointments;
   }
 
   public findByDate(date: Date): Appointment | null {
@@ -16,10 +25,10 @@ class AppointmentRepository {
     return findAppointment || null;
   }
 
-  public create(provider: string, date: Date): Appointment {
-    const appointment = new Appointment(provider, date);
+  public create({ date, provider }: CreateAppointmentDTO): Appointment {
+    const appointment = new Appointment({ provider, date });
 
-    this.appointment.push(appointment);
+    this.appointments.push(appointment);
 
     return appointment;
   }
