39cbd6c7f084765b4cc8c3fc844552e8037a5ae1
Lucas Viana <lucasviana112@gmail.com>
2020-06-30 23:06:07 -0300

    adaptando a rota para uso de models do banco definidos pelo typeorm

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,29 +1,29 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
 
 import AppointmentRepository from '../repositories/AppointmentRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentRepository = new AppointmentRepository();
 
-appointmentsRouter.get('/', (request, response) => {
-  const appointments = appointmentRepository.all();
+appointmentsRouter.get('/', async (request, response) => {
+  const appointmentRepository = getCustomRepository(AppointmentRepository);
+  const appointments = await appointmentRepository.find();
 
   return response.json(appointments);
 });
 
-appointmentsRouter.post('/', (request, response) => {
+appointmentsRouter.post('/', async (request, response) => {
   try {
     const { provider, date } = request.body;
 
     // converte string para Date, e "arredonda" a hora
     const parsedDate = parseISO(date);
-    const createAppointment = new CreateAppointmentService(
-      appointmentRepository,
-    );
 
-    const appointment = createAppointment.execute({
+    const createAppointment = new CreateAppointmentService();
+
+    const appointment = await createAppointment.execute({
       date: parsedDate,
       provider,
     });
