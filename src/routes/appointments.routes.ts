7da5c62d8181d3e96e97c6d6f75468bdfeacbdeb
Lucas Viana <lucasviana112@gmail.com>
2020-07-03 16:47:28 -0300

    alteração provider e chamada do autenticador

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -5,7 +5,11 @@ import { getCustomRepository } from 'typeorm';
 import AppointmentRepository from '../repositories/AppointmentRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
 const appointmentsRouter = Router();
+
+appointmentsRouter.use(ensureAuthenticated);
 
 appointmentsRouter.get('/', async (request, response) => {
   const appointmentRepository = getCustomRepository(AppointmentRepository);
@@ -15,23 +19,23 @@ appointmentsRouter.get('/', async (request, response) => {
 });
 
 appointmentsRouter.post('/', async (request, response) => {
-  try {
-    const { provider, date } = request.body;
+  // try {
+  const { provider_id, date } = request.body;
 
-    // converte string para Date, e "arredonda" a hora
-    const parsedDate = parseISO(date);
+  // converte string para Date, e "arredonda" a hora
+  const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService();
+  const createAppointment = new CreateAppointmentService();
 
-    const appointment = await createAppointment.execute({
-      date: parsedDate,
-      provider,
-    });
+  const appointment = await createAppointment.execute({
+    date: parsedDate,
+    provider_id,
+  });
 
-    return response.json(appointment);
-  } catch (err) {
-    return response.status(400).json({ error: err.message });
-  }
+  return response.json(appointment);
+  // } catch (err) {
+  //   return response.status(400).json({ error: err.message });
+  // }
 });
 
 export default appointmentsRouter;
