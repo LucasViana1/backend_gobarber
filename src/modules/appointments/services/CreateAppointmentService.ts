4614ab44bf806e017e67ebf041d57efbbd2c302b
Lucas Cunha <lucasviana112@gmail.com>
2020-07-17 00:59:41 -0300

    estrutura de pastas inicial da metodologia DDD, correção de algumas importações

--- src/modules/appointments/services/CreateAppointmentService.ts
+++ src/modules/appointments/services/CreateAppointmentService.ts
@@ -1,7 +1,7 @@
 import { startOfHour } from 'date-fns';
 import { getCustomRepository } from 'typeorm';
 
-import Appointment from '../models/Appointment';
+import Appointment from '../infra/typeorm/entities/Appointment';
 import AppointmentRepository from '../repositories/AppointmentRepository';
 
 interface RequestDTO {
