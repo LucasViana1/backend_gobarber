018b05c03a3def06f60f080e0f6c1f19858062e4
Lucas Cunha <lucasviana112@gmail.com>
2020-08-13 21:51:42 -0300

    created MailProvider (Fake and Interface)

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -9,6 +9,9 @@ import AppointmentRepository from '@modules/appointments/infra/typeorm/repositor
 import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
 
+// import IUserTokenRepository from '@modules/users/repositories/IUserTokenRepository';
+// import UsersRepository from '@modules/users/infra/typeorm/repositories/UsersRepository';
+
 container.registerSingleton<IAppointmentsRepository>(
   'AppointmentRepository',
   AppointmentRepository,
--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,6 +3,8 @@ import { container } from 'tsyringe';
 import IStorageProvider from './StorageProvider/models/IStorageProvider';
 import DiskStorageProvider from './StorageProvider/implementations/DiskStorageProvider';
 
+// import DiskStorageProvider from './mai/implementations/DiskStorageProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
